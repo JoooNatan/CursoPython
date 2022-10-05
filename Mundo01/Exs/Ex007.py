379f3908729c40652054e611b666c72f39012931
João Natan <joaonatan67548@gmail.com>
2022-10-05 20:43:20 -0300

    comentarios

--- Mundo01/Exs/Ex007.py
+++ Mundo01/Exs/Ex007.py
@@ -1,3 +1,3 @@
-n1 = int(input('Digite a primeira nota '))
-n2 = int(input('Digite a segunda nota '))
-print('Media: {}'.format((n1 + n2) / 2))
+n1 = float(input('Digite a primeira nota '))
+n2 = float(input('Digite a segunda nota '))
+print('Media: {:.1f}'.format((n1 + n2) / 2))
