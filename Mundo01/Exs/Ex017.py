9267f2b51111a021384811ba6a35a9f5e6fd1172
João Natan <joaonatan67548@gmail.com>
2022-10-07 12:46:54 -0300

    feito

---
+++ Mundo01/Exs/Ex017.py
@@ -0,0 +1,6 @@
+from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+#hip = ((co ** 2 + ca ** 2) ** (1 / 2))
+hip = hypot(co, ca)
+print('A medida da hipotenusa vai medir {:.2f}'.format(hip))
