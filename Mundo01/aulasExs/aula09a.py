27a914ab8bdf74a915bd426286902b39d1caa44b
João Natan <joaonatan67548@gmail.com>
2022-10-10 15:31:01 -0300

    aula 09

---
+++ Mundo01/aulasExs/aula09a.py
@@ -0,0 +1,10 @@
+frase = 'Curso em Video Python'
+print(frase[2::2])
+print(frase.count('o'))
+print(frase.upper())
+print(frase.lower())
+print(frase.title())
+print(frase.replace('Python', 'Chico'))
+print('Curso' in frase)
+div = frase.split()
+print(div[0])
