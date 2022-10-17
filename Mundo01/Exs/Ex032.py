3f229e48d679bc45968b6598c4f6457b24359da2
João Natan <joaonatan67548@gmail.com>
2022-10-17 16:55:56 -0300

    feito

--- Mundo01/Exs/Ex032.py
+++ Mundo01/Exs/Ex032.py
@@ -1,1 +1,8 @@
-ano = int(input('Digite um ano para saber se ele é bissexto '))
+from datetime import date
+ano = int(input('Digite um ano para saber se ele é bissexto ou não: '))
+if ano == 0:
+    ano = date.today().year#Pega o ano atual
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} não é BISSEXTO'.format(ano))
