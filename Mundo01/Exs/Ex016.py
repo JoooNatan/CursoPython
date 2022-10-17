ee654b1728a4edec3184d21f97296c952e747964
João Natan <joaonatan67548@gmail.com>
2022-10-17 13:53:08 -0300

    feito

--- Mundo01/Exs/Ex016.py
+++ Mundo01/Exs/Ex016.py
@@ -3,6 +3,7 @@ import math
 '''from math import trunc
 num = float(input('Digite um numero real '))
 print('O número {} tem a parte Inteira {}.'.format(num, trunc(num)))'''
+
 num = float(input('Digite um numero real '))
 print('O número {} tem a parte Inteira {}.'.format(num, int(num)))
  
