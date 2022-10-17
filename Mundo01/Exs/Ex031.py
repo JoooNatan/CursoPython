b7315e1a1d1995f8c732b578b22ff5e03cf984b7
João Natan <joaonatan67548@gmail.com>
2022-10-17 16:45:54 -0300

    feito

--- Mundo01/Exs/Ex031.py
+++ Mundo01/Exs/Ex031.py
@@ -3,3 +3,6 @@ if dis <= 200:
     print('O preço da passagem sera de: R${}'.format(dis * 0.50))
 else:
     print('O preço da passagem sera de: R${}'.format(dis * 0.45))
+
+#if simplificado
+#preco = dis * 0.50 if dis <= 200 else preco = dis * 0.45
