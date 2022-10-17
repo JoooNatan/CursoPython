42f8ee240ed01cbfa94fb618bfad6fac221e3f32
João Natan <joaonatan67548@gmail.com>
2022-10-17 13:51:46 -0300

    aula 10

---
+++ Mundo01/aulasExs/aula10a.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome '))
+if nome == 'Joao':
+    print('Que nome lindo voce tem')
+print('Bom dia {}'.format(nome))
---
+++ Mundo01/aulasExs/aula10b.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite a primeira nota '))
+n2 = float(input('Digite a segunda nota '))
+m = (n1 + n2) / 2
+print('A media foi: {}'.format(m))
+if m >= 6:
+    print('Sua media foi boa!')
+else:
+    print('Sua media nao foi boa')
