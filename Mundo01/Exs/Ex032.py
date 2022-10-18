36278194c47023e1a7b498c1445436fac6329ce1
João Natan <joaonatan67548@gmail.com>
2022-10-18 16:21:21 -0300

    feito

--- Mundo01/Exs/Ex032.py
+++ Mundo01/Exs/Ex032.py
@@ -3,6 +3,6 @@ ano = int(input('Digite um ano para saber se ele é bissexto ou não: '))
 if ano == 0:
     ano = date.today().year#Pega o ano atual
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-    print('O ano {} é BISSEXTO'.format(ano))
+    print('O ano {} é \033[32mBISSEXTO\033[m'.format(ano))
 else:
-    print('O ano {} não é BISSEXTO'.format(ano))
+    print('O ano {} não é \033[31mBISSEXTO\033[m'.format(ano))
