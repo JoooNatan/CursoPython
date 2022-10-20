b2e9ec5a8ecefb9ddf07f68dc424b38be0f6297e
João Natan <joaonatan67548@gmail.com>
2022-10-20 14:32:50 -0300

    feito

--- Mundo01/Exs/Ex035.py
+++ Mundo01/Exs/Ex035.py
@@ -7,4 +7,4 @@ r3 = float(input('Digite o segmento da 3° reta: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima \033[1;32mpodem formar um TRIANGULO!!!\033[m')
 else:
-    print('Os segmentos acima \033[1;31mNÃO podem formar um TRIANGULO!!!\033[m')
+    print('Os segmentos acima \033[1;31mNÃO podem formar um TRIANGULO!!!\033[m') 
---
+++ Mundo01/aulasExs/aula12a.py
@@ -0,0 +1,11 @@
+nome = str(input('Digite seu nome '))
+if nome == 'Joao':
+    print('Que nome bonito')
+elif nome == 'Paulo' or nome == 'Ana' or nome == 'Pedro':
+    print('Seu nome é bem popular no brasil')
+elif nome == 'Maria':
+    print('Ola maria')
+else:
+    print('Seu nome é bem comum')
+print('Tenha um bom dia!')
+#estrutura aninhada
