ac4c091706e1d0f9fded66e39efc235292c5f59f
João Natan <joaonatan67548@gmail.com>
2022-10-20 14:49:27 -0300

    feito

---
+++ Mundo01/Exs/Ex036.py
@@ -0,0 +1,17 @@
+print('-=' * 20)
+print('         Emprestimo bancario')
+print('-=' * 20)
+
+valorCasa = float(input('Qual o valor da casa? '))
+salario = float(input('Qual o seu salario? '))
+anos = int(input('Digite em quantos anos voce ira pagar: '))
+prestacaoMensal = float(valorCasa / (anos * 12))
+
+print('-' * 30)
+print('Situaçao do emprestimo:')
+
+if prestacaoMensal > (salario * 0.30):
+    print('\033[31mNegado!\033[m')
+else:
+    print('\033[32mAprovado!\033[m')
+print('Valor das prestacões mensais: R${:.2f}'.format(prestacaoMensal))
