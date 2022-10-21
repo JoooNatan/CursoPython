b60c1d40fdb8a14afc9c77ec4b47d749db7ca4a8
João Natan <joaonatan67548@gmail.com>
2022-10-21 11:38:44 -0300

    feito

--- Mundo02/Exs/Ex037.py
+++ Mundo02/Exs/Ex037.py
@@ -1,5 +1,17 @@
-import math
-print('-=' * 20)
-print(' Conversor de base(2 -> 10 e 10 -> 2)')
-print('-=' * 20)
-num = int(input('Digite um numero intero '))
+print('-=' * 10)
+print('Conversor de bases')
+print('-=' * 10)
+num = int(input('Digite um numero intero: '))
+print('''Escolha a base para converção
+[1] para BINARIO
+[2] para OCTAL
+[3] para HEXADECIMAL''')
+op = int(input('Opção: '))
+if op == 1:
+    print('{} em binario é igual a : {}'.format(num, bin(num)[2:]))
+elif op == 2:
+    print('{} em octal é igual a : {}'.format(num, oct(num)[2:]))
+elif op == 3:
+    print('{} em hexadecimal é igual a : {}'.format(num, hex(num)[2:]))
+else:
+    print('Opção invalida!')
