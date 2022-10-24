20500c2974c1d6ca6d198fd209d8d08c922b5cdc
João Natan <joaonatan67548@gmail.com>
2022-10-23 21:16:26 -0300

    aula 13

---
+++ Mundo02/aulasExs/aula13a.py
@@ -0,0 +1,16 @@
+for c in range(1, 21, 3):#range == intervalo | terceiro numero == passo
+    if c % 2 == 0:
+        print('[{}]'.format(c), end=' ')
+    else:
+        print(c, end=' ')
+print('Fim')
+
+s = 0
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f + 1, p):
+    print(c, end = ' ')
+    s = s + c
+
+print('Soma dos valores: {}'.format(s))
