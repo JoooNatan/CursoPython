342712dd12824ae1f52baedab37d639e7b4c99ba
João Natan <joaonatan67548@gmail.com>
2022-11-04 23:18:11 -0300

    opcoes

---
+++ Mundo02/Exs/Ex059.py
@@ -0,0 +1,25 @@
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+op = 0
+
+while op != 5:
+    if op > 5:
+        print('\033[31mOpção invalida\033[m! digite novamente')
+
+    print('[ 1 ] somar\n[ 2 ] multiplicar\n[ 3 ] maior\n[ 4 ] novos números\n[ 5 ] sair do programa')
+    op = int(input('Opção: '))
+
+    if op == 1:
+        print('Resultado: {}'.format(n1 + n2))
+    elif op == 2:
+        print('Resultado: {}'.format(n1 * n2))
+    elif op == 3:
+        print('Maior numero: ', end = '')
+        if n1 > n2:
+            print(n1)
+        else:
+            print(n2)
+    elif op == 4:
+        n1 = int(input('Digite o primeiro valor: '))
+        n2 = int(input('Digite o segundo valor: '))
+    print('-' * 30)
