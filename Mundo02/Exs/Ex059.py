e13d1c18da2aa2b8b0292d39b89a30f6c69c1608
João Natan <joaonatan67548@gmail.com>
2022-11-05 18:29:30 -0300

    opcoes

--- Mundo02/Exs/Ex059.py
+++ Mundo02/Exs/Ex059.py
@@ -10,10 +10,10 @@ while op != 5:
     op = int(input('Opção: '))
 
     if op == 1:
-        print('Resultado: {}'.format(n1 + n2))
+        print('{} + {} = {}'.format(n1, n2, (n1 + n2)))
 
     elif op == 2:
-        print('Resultado: {}'.format(n1 * n2))
+        print('{} x {} = {}'.format(n1, n2, (n1 * n2)))
 
     elif op == 3:
         print('Maior numero: ', end = '')
