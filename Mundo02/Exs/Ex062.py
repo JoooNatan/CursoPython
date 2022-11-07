916542a706a828f37b3167566106b5c95050cc58
João Natan <joaonatan67548@gmail.com>
2022-11-06 22:43:51 -0300

    PA

---
+++ Mundo02/Exs/Ex062.py
@@ -0,0 +1,16 @@
+primeiro = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razão: '))
+termo = primeiro
+cont = 1
+tot = 0
+mais = 10
+
+while mais != 0:
+    tot = tot + mais
+    while cont <= tot:
+        print('{} -> '.format(termo), end = '')
+        termo += razao
+        cont += 1
+    print('Pausa')
+    mais = int(input('Quantos termos voce quer mostrar a mais? '))
+print('Fim')
