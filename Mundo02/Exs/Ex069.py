62461d412653a164d31c37431fa8e6e5dbaad2cc
João Natan <joaonatan67548@gmail.com>
2022-11-15 18:56:19 -0300

    Análise de dados do grupo

--- Mundo02/Exs/Ex069.py
+++ Mundo02/Exs/Ex069.py
@@ -8,14 +8,18 @@ while True:
         print('Cadastre uma Pessoa')
         print('-' * 30)
         idade = int(input('Digite a idade: '))
-        sexo = str(input('Digite o sexo:[M/F] ')).upper()
-        if idade > 18:
+        while sexo in 'MF':
+            sexo = str(input('Digite o sexo:[M/F] ')).strip().upper()[0]
+
+        if idade >= 18:
             pessoa18mais += 1
         if sexo == 'M':
             homens += 1
         if sexo == 'F' and idade < 20:
             mulher20menos += 1
-        op = str(input('Quer Continuar?[S/N] ')).upper()
+
+        while op not in 'SN':
+            op = str(input('Quer Continuar?[S/N] ')).strip().upper()[0]
 
 print(f'{pessoa18mais} pessoas tem mais de 18 anos')
 print(f'{homens} homens foram cadastrados')
