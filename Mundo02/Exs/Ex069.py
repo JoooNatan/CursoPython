354ab0aec617d5b4b09c17f0b412f08ce50da67a
João Natan <joaonatan67548@gmail.com>
2022-11-16 18:44:58 -0300

    Análise de dados do grupo

--- Mundo02/Exs/Ex069.py
+++ Mundo02/Exs/Ex069.py
@@ -1,14 +1,15 @@
-op = 'S'
+op = ' '
 pessoa18mais = homens = mulher20menos = 0
 while True:
     if 'N' in op:
         break
     else:
         print('-' * 30)
-        print('Cadastre uma Pessoa')
+        print('{:-^30}'.format(' Cadastre uma Pessoa '))
         print('-' * 30)
         idade = int(input('Digite a idade: '))
-        while sexo in 'MF':
+        sexo = ' '
+        while sexo not in 'MF':
             sexo = str(input('Digite o sexo:[M/F] ')).strip().upper()[0]
 
         if idade >= 18:
@@ -17,7 +18,8 @@ while True:
             homens += 1
         if sexo == 'F' and idade < 20:
             mulher20menos += 1
-
+            
+        op = ' '
         while op not in 'SN':
             op = str(input('Quer Continuar?[S/N] ')).strip().upper()[0]
 
---
+++ Mundo02/Exs/Ex070.py
@@ -0,0 +1,25 @@
+tot = totMil = menorPreco = cont = 0
+barato = ''
+while True:
+    produto = str(input('Nome do produto: '))
+    preco = float(input('Preco: '))
+    cont += 1
+    tot += preco
+
+    if preco > 1000:
+        totMil += 1
+
+    if cont == 1 or preco < menorPreco:
+        menorPreco = preco
+        barato = produto
+
+    res = ' '
+    while res not in 'SN':
+        res = str(input('Quer continuar?[S/N] ')).strip().upper()[0]
+    if res == 'N':
+        break
+
+print('{:-^40}'.format(' FIM '))
+print(f'O total da compra foi {tot:.2f}')
+print(f'{totMil} produtos custam mais de R$1000')
+print(f'O produto mais barato foi {barato} e custou R${menorPreco:.2f}')
