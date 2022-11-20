e8c93825a8c4f234011be44966d350ad0d09cf36
João Natan <joaonatan67548@gmail.com>
2022-11-19 23:09:12 -0300

    Número por Extenso

--- Mundo03/Exs/Ex072.py
+++ Mundo03/Exs/Ex072.py
@@ -1,11 +1,14 @@
 nums = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
 'onze', 'doze', 'treze', 'quartoze', 'quinze', 'dezeseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
 
+op = 'S'
 while True:
-    n = int(input('Digite um numero entre 0 e 20: '))
-    if 0 <= n <= 20:
+    if op == 'S':
+        n = int(input('Digite um numero entre 0 e 20: '))
+        if n > 20 or n < 0:
+            print('Tente novamente. ', end = '')
+        else:
+            print(f'Voce digitou o numero {nums[n]}')
+            op = str(input('Quer Continuar?[S/N] ')).strip().upper()[0]
+    else:
         break
-    
-    print('Tente novamente. ', end = '')
-
-print(f'Voce digitou o numero {nums[n]}')
