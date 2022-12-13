7226ca2f7afb4e80949b4009eff3004b27263870
João Natan <joaonatan67548@gmail.com>
2022-12-12 23:46:02 -0300

    Palpites para a Mega Sena

---
+++ Mundo03/Exs/Ex088.py
@@ -0,0 +1,31 @@
+from random import randint
+from time import sleep
+
+lista = []
+jogos = []
+tot = 1
+
+print('-' * 30)
+print(f'{"JOGA NA MEGA SENA":^30}')
+print('-' * 30)
+quant = int(input('Quantos jogos para sortear? '))
+
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+
+print(f'====== Sorteando {quant} jogos ======')
+for i, l in enumerate(jogos):
+    print(f'jogo {i + 1}: {l}')
+    sleep(0.5)
