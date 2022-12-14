8ea319a3c7ef8d2e967601e12303d8c8ca268b34
João Natan <joaonatan67548@gmail.com>
2022-12-14 17:38:34 -0300

    Boletim com listas compostas

--- Mundo03/Exs/Ex089.py
+++ Mundo03/Exs/Ex089.py
@@ -11,20 +11,23 @@ while True:
         break
     print('-' * 23)
 
-print('-' * 23)
-print('No.    NOME       MEDIA')
+print('-' * 38)
+print('No.    NOME       MEDIA       SITUAÇÃO')
 for i, a in enumerate(alunos):
     print(f'{i:<7}', end = '')
     print(f'{a[0]:<11}', end = '')
-    print(f'{a[3]:>5.1f}')
-print('-' * 23)
+    print(f'{a[3]:>5.1f}', end = '')
+    print(f'{"Aprovado":>15}' if a[3] >= 7 else f'{"Reprovado":>15}')
+print('-' * 38)
 
-while True:
-    nAluno = int(input('Mostrar notas de qual aluno?(999 interrompe) '))
-    if nAluno == 999:
-        break
-    while nAluno >= len(alunos) or nAluno < 0:
-        nAluno = int(input('Numero invalido! Digite novamente: '))
+res = str(input('Quer consultar as notas?[S/N] '))
+if res in 'Ss':
+    while True:
+        nAluno = int(input('Mostrar notas de qual aluno?(999 interrompe) '))
+        if nAluno == 999:
+            break
+        while nAluno >= len(alunos) or nAluno < 0:
+            nAluno = int(input('Numero invalido! Digite novamente: '))
 
-    print(f'Notas de {alunos[nAluno][0]} são {alunos[nAluno][1]} e {alunos[nAluno][2]}')
-    print('-' * 46)
+        print(f'Notas de {alunos[nAluno][0]} são {alunos[nAluno][1]} e {alunos[nAluno][2]}')
+        print('-' * 46)
