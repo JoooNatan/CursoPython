ec6a54f9c975e25de90d793ddde09f9d716c9d38
João Natan <joaonatan67548@gmail.com>
2022-12-17 17:03:49 -0300

    Dicionários em Python

---
+++ Mundo03/Exs/Ex090.py
@@ -0,0 +1,12 @@
+aluno = {}
+
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Media de {aluno["nome"]}: '))
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+else:
+    aluno['situacao'] = 'Reprovado'
+
+print('-' * 20)
+for k, v in aluno.items():
+    print(f'{k} é igual a {v}')
