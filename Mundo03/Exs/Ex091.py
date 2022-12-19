db50834d9a33b64eba1da0e3de68a4c01d5976ca
João Natan <joaonatan67548@gmail.com>
2022-12-19 18:50:10 -0300

    Jogo de Dados em Python

---
+++ Mundo03/Exs/Ex091.py
@@ -0,0 +1,19 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
+ranking = []
+jogo = {'jogador1': randint(1, 6),
+'jogador2': randint(1, 6),
+'jogador3': randint(1, 6),
+'jogador4': randint(1, 6)}
+
+print('Valores sorteados: ')
+for k, v in jogo.items():
+    print(f'{k} tirou {v}')
+    sleep(0.5)
+
+ranking = sorted(jogo.items(), key = itemgetter(1), reverse = True)#itemgetter = pega o valor
+print('-' * 25)
+print('==RANKING DOS JOGADORES==')
+for i, v in enumerate(ranking):
+    print(f'{i + 1}° lugar: {v[0]} com {v[1]}.')
